241ac2d88038e3f68d5109edce431e59350408d2
padlesky <padlesky@ualberta.ca>
2015-01-13 09:13:11 -0700

    Started Repository for lab 1

---
+++ exercise1.py
@@ -0,0 +1,26 @@
+class Student:
+    courseMarks={}
+    name = ""
+    family = ""
+
+    def _init_(self, name, family):
+        self.name = name
+        self.family = family
+
+    def addCourseMark(self, course, mark):
+        self.courseMarks[course] = mark
+
+    def average(self):
+        temp = 0
+        count = 0
+        for x in self.courseMarks:
+            temp = temp + self.courseMarks[x]
+            count = count + 1
+        avg = temp/count
+        print avg
+
+student = Student()
+student.addCourseMark("CMPUT310", 80)
+student.addCourseMark("STS351", 70)
+student.addCourseMark("CMPUT350", 80)
+student.average()
